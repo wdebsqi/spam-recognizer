4681a5c511c32b5d2b45e18391654bdcbd36cb17
Kinimod12 <dominik.bilski42@gmail.com>
2021-05-27 17:27:38 +0200

    Add TextNormalizer class.

---
+++ text_normalizer.py
@@ -0,0 +1,26 @@
+import re
+
+class TextNormalizer:
+    def normalize(self, original_text):
+        lowered_text = self.tranform_to_lowercase(original_text)
+        
+        alphanumeric_text = self.remove_non_alphanumeric_chars(lowered_text)
+        
+        text_with_correct_whitespaces = self.remove_redundant_whitespaces(alphanumeric_text)
+
+        fully_normalized_text = self.remove_stopwords(text_with_correct_whitespaces)
+    
+        return fully_normalized_text
+    
+    def transform_to_lowercase(self, text):
+        return text.lower()
+    
+    def remove_non_alphanumeric_chars(self, text):
+        return re.sub(r'[^\d\w\s]', '', text)
+    
+    def remove_redundant_whitespaces(self, text):
+        return ' '.join([word for word in text.split() if word != ' '])
+    
+    def remove_stopwords(self, text):
+        stop = stopwords.words('english')
+        return ' '.join([word for word in text.split() if word not in stop])

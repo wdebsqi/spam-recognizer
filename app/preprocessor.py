9b8f9c86900bf3bafc27eacb2b865b8a550fcb8f
Kinimod12 <dominik.bilski42@gmail.com>
2021-05-27 19:13:16 +0200

    Add TestPreprocessor. Download stopwords in preprocessor only when missing.

--- app/preprocessor.py
+++ app/preprocessor.py
@@ -9,17 +9,22 @@ class Preprocessor:
             'ham': 0,
             'spam': 1
         }
-        nltk.download('stopwords')
-        english_stopwords = stopwords.words('english')
+
+        try:
+            english_stopwords = stopwords.words('english')
+        except LookupError:
+            nltk.download('stopwords')
+            english_stopwords = stopwords.words('english')
+        
         self.text_normalizer = text_normalizer.TextNormalizer(stopwords_to_exclude=english_stopwords)
 
-    def preprocess_data(self, source_data):
+    def preprocess_data(self, source_data, column_mapping):
         data_with_clean_text = self.clean_column_containing_text(
             dataset=source_data,
-            column_name='message')
+            column_name=column_mapping['text_column'])
         preprocessed_data = self.normalize_labels(
             dataset=data_with_clean_text,
-            column_name='label')
+            column_name=column_mapping['label_column'])
         return preprocessed_data
     
     def clean_column_containing_text(self, dataset, column_name):
@@ -27,6 +32,5 @@ class Preprocessor:
         return dataset
     
     def normalize_labels(self, dataset, column_name):
-        dataset_to_return = dataset.copy(deep=True)
-        dataset_to_return[column_name] = dataset.get(column_name).map(self.label_text_to_int_mapping)
+        dataset[column_name] = dataset.get(column_name).map(self.label_text_to_int_mapping)
         return dataset
